0eaa004b30eba4b9cc5749d657a5e3ff3f6b486f
uniq47 <usapkota47@gmail.com>
2023-10-10 00:49:42 -0700

    fetched products and categories

---
+++ store/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.store, name = 'store'),
+    path('product/<slug:product_slug>', views.product_info, name = 'product-info'),
+    path('category/<slug:category_slug>', views.list_category, name = 'list-category'),
+
+]
+
+
