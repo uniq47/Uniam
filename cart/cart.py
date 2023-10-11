dcedc03804fef1a6660b4410bba8ea59f36098d9
uniq47 <usapkota47@gmail.com>
2023-10-11 09:09:45 -0700

    added cart

--- cart/cart.py
+++ cart/cart.py
@@ -1,12 +1,12 @@
-class Cart():
-    def __init__(self, request):
-        self.session = request.session
-        #obrain returing users existing session
-        cart = self.session.get('session_key')
+# class Cart():
+#     def __init__(self, request):
+#         self.session = request.session
+#         #obrain returing users existing session
+#         cart = self.session.get('session_key')
         
-        # generate new session for new user
-        if 'session_key' not in request.session:
-            cart = self.session['session_key'] = {}
-        self.cart = cart
+#         # generate new session for new user
+#         if 'session_key' not in request.session:
+#             cart = self.session['session_key'] = {}
+#         self.cart = cart
             
         
